ea50a52b021b0acbc587f85e20b23be59d06a4e6
CHIMEZIRI FREEDOM <167892201+terryfreezy@users.noreply.github.com>
2024-08-31 06:40:52 -0700

    upload

---
+++ manuel.js
@@ -0,0 +1,45 @@
+// console.log
+// console.error("welcome")
+// console.warn("welcome")
+
+// let emkaAge = 56;
+// emkaAge = 21;
+// emkaAge = 36;
+// console.log(emkaAge);
+// const emkaAge = 67;
+// console.log(emkaAge);
+// let score;
+// score = "welcome";
+// score = "tony"
+// console.log(score);n
+
+// datatypes
+// const firstname="Peter";
+// const lastname="Obi";
+// const age=63;
+// const isAdimin = false;
+// const x = null;
+// const y = undefined;
+// console.log(typeof y);
+// console.log("my name is " + firstname + lastname + "i am" + age + "years old");
+// console.log(`my name ${firstname} ${lastname} ${age} years old ${isAdiminh}`);
+
+// const tools = "knife, book, pencil, crayon";
+// console.log(tools.split(","));
+// // console.log(word.length);
+
+// const listitems=["Beans","rice","garri",40, true, null, undefined];
+// listitems.push("goat",58, "computer",)
+// listitems.pop()
+// listitems.unshift("pen","chalk")
+// console.log(listitems);
+// console.log(listitems[`4`]);
+
+// const studentinfo={
+//     firstname:"favour",
+//     lastname:"amaka",
+//     age:13,
+//     hobbies:["dancing", "sports","twerking",89],haspaidschoolfees:true,
+//     x:null, iswicked:undefined,
+// };
+// console.log(studentinfo.x);
